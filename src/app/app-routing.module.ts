228239003a24d0c34a5683f210420901bd1e45e5
Suleiman Sani <suleiman.sani1@gmail.com>
2021-10-16 22:09:33 +0100

    default home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'project',component:ProjectComponent},
   {path:'contact',component:ContactComponent},
+  {path:"", redirectTo:"home",pathMatch:'full'}
 ];
 
 @NgModule({
